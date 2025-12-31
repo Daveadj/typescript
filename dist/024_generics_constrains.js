4218ae3f7a5a532bad4f51382f13a60829118ca7
David Adjerese <ovoadjerese@gmail.com>
2025-12-31 17:51:26 +0100

    update

--- dist/024_generics_constrains.js
+++ dist/024_generics_constrains.js
@@ -8,4 +8,15 @@ function lenN4(xN4) {
 console.log(lenN4([1, 2, 3, 4]));
 console.log(lenN4("Hello"));
 console.log(lenN4({ length: 10, tag: "array" }));
-// console.log(lenN4(1)); //1 doesn't have a length property
+function userN6Extract(arrN4, keyN4) {
+    return arrN4.map(item => item[keyN4]);
+}
+const usersN6 = [
+    {
+        id: '1', name: 'name1', age: 22
+    },
+    {
+        id: '2', name: 'name2'
+    }
+];
+console.log(userN6Extract(usersN6, "id"));
